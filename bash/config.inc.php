ce98587f50fe77a46e02fb4f848e7e98c936fb09
Farindra <farindra.project@gmail.com>
2016-05-29 18:20:54 +0700

    Update config.inc.php

--- bash/config.inc.php
+++ bash/config.inc.php
@@ -11,7 +11,7 @@
 // For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
 // NOTE: for SQLite use absolute path: 'sqlite:////full/path/to/sqlite.db?mode=0646'
 //$config['db_dsnw'] = 'mysql://roundcube:password@localhost/roundcube';
-$config['db_dsnw'] ='%%mysql%%'
+$config['db_dsnw'] ='%%mysql%%';
 // you can define specific table (and sequence) names prefix
 $config['db_prefix'] = '%%prefix%%';
 
